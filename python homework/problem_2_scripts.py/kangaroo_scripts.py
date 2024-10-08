b170992acd0f4cf9d1f9c06e358c9f341c176314
evantheodar <evanthedor545@gmail.com>
2024-10-08 12:34:29 +0530

    Update kangaroo_scripts.py

--- python homework/problem_2_scripts.py/kangaroo_scripts.py
+++ python homework/problem_2_scripts.py/kangaroo_scripts.py
@@ -1,1 +1,14 @@
 
+def kangaroo(x1, v1, x2, v2):
+
+    if v1 == v2:
+        return "YES" if x1 == x2 else "NO"
+    elif (x2 - x1) * (v1 - v2) > 0 and (x2 - x1) % (v1 - v2) == 0:
+        return "YES"
+    else:
+        return "NO"
+
+# Example usage:
+x1, v1, x2, v2 = map(int, input().split())  # Input values
+print(kangaroo(x1, v1, x2, v2))
+
