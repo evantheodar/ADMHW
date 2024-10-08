c078b156e6f2550c6404e9a8b0fa831664a902bc
evantheodar <evanthedor545@gmail.com>
2024-10-08 11:55:04 +0530

    Update regrex_and_parsing_scripts.py

--- python homework/problem_1_scripts.py/regrex_and_parsing_scripts.py
+++ python homework/problem_1_scripts.py/regrex_and_parsing_scripts.py
@@ -1,1 +1,48 @@
 
+###1 Detect Floating Point Number
+
+
+###2 Re.split()
+
+
+###3 Group(), Groups() & Groupdict()
+
+
+###4 Re.findall() & Re.finditer()
+
+
+###5 Re.start() & Re.end()
+
+
+###6 Regex Substitution
+
+
+###7 Validating Roman Numerals
+
+
+###8 Validating phone numbers
+
+.
+###9 Validating and Parsing Email Addresses
+
+
+###10 Hex Color Code
+
+
+###11 HTML Parser - Part 1
+
+
+###12 HTML Parser - Part 2
+
+
+###13 Detect HTML Tags, Attributes and Attribute Values
+
+
+###14 Validating UID
+
+
+###15 Validating Credit Card Numbers
+
+
+###16 Validating Postal Codes 
+
