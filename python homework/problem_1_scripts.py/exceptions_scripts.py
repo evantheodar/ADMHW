eaa2fa9e44b77d9d589a53b23782ce6c9be7bf53
evantheodar <evanthedor545@gmail.com>
2024-10-08 11:25:54 +0530

    Update exceptions_scripts.py

--- python homework/problem_1_scripts.py/exceptions_scripts.py
+++ python homework/problem_1_scripts.py/exceptions_scripts.py
@@ -1,1 +1,24 @@
+ ###1 Exceptions
+
+def integer_division(a, b):
+    try:
+        # Attempt integer division
+        result = a // b
+        return result
+    except ZeroDivisionError as e:
+      
+        return "Error Code: " + str(e)
+    except ValueError as e:
+       
+        return "Error Code: " + str(e)
+
+T = int(input())
+
+for _ in range(T):
+    try:
+        a, b = map(int, input().split())
+        print(integer_division(a, b))
+    except ValueError as e:
+        print("Error Code: " + str(e))
+
 
