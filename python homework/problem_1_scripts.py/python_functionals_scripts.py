f60488397de961400edfcc1fdda30e453cd632e5
evantheodar <evanthedor545@gmail.com>
2024-10-08 11:30:53 +0530

    Update python_functionals_scripts.py

--- python homework/problem_1_scripts.py/python_functionals_scripts.py
+++ python homework/problem_1_scripts.py/python_functionals_scripts.py
@@ -1,1 +1,13 @@
 
+# 1 Map and Lambda Function
+# Function to generate the first N Fibonacci numbers
+def fibonacci(n):
+    fib_seq = [0, 1]
+    for i in range(2, n):
+        fib_seq.append(fib_seq[-1] + fib_seq[-2])
+    return fib_seq[:n]
+
+# Define a cube function to cube each Fibonacci number
+def cube(x):
+    return x ** 3
+
